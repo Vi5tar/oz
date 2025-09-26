6c00bfa5077eb46e014c36f179e6aaf8334b965a
Travis Swiers <tswiers@us.ci.org>
2025-09-26 16:21:38 -0600

    feat: select profile to login with

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { STSClient, GetCallerIdentityCommand } from '@aws-sdk/client-sts'
 import { Command, Option } from 'commander'
 import { fromIni } from '@aws-sdk/credential-providers'
 import { writeSync } from 'node:fs'
+import { select } from '@inquirer/prompts'
 
 const exec = util.promisify(childProcess.exec)
 const program = new Command()
@@ -17,25 +18,40 @@ program
   .description(
     "Login to AWS with the given profile and sets AWS_PROFILE or AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN, and AWS_CREDENTIAL_EXPIRATION. (Doesn't log in again if already logged in.)"
   )
-  .requiredOption('-p, --profile <string>', 'the aws profile to log in with (~/.aws/config)')
+  .option('-p, --profile <string>', 'the aws profile to log in with (~/.aws/config)')
   .addOption(
     new Option('-e, --export <string>', 'which environment variables to export.')
       .choices(['profile', 'creds', 'none'])
       .default('profile')
   )
   .action(async (args) => {
+    let { profile } = args
+
+    if (!profile) {
+      const { stdout } = await exec('aws configure list-profiles')
+      const profiles = stdout.trim().split('\n')
+
+      profile = await select({
+        message: 'Select a profile',
+        choices: profiles.map((profile) => ({
+          name: profile,
+          value: profile
+        }))
+      })
+    }
+
     try {
-      const { stdout: region } = await exec(`aws configure get region --profile ${args.profile}`)
+      const { stdout: region } = await exec(`aws configure get region --profile ${profile}`)
 
       const stsClient = new STSClient({
         region: region.trim(),
-        credentials: fromIni({ profile: args.profile })
+        credentials: fromIni({ profile })
       })
 
       await stsClient.send(new GetCallerIdentityCommand({}))
     } catch (error) {
       try {
-        const awsLogin = spawn('aws', ['sso', 'login', '--profile', args.profile])
+        const awsLogin = spawn('aws', ['sso', 'login', '--profile', profile])
         awsLogin.stdout.on('data', (data) => {
           console.log(data.toString())
         })
@@ -53,11 +69,11 @@ program
       writeSync(3, 'unset AWS_SECRET_ACCESS_KEY\n')
       writeSync(3, 'unset AWS_SESSION_TOKEN\n')
       writeSync(3, 'unset AWS_CREDENTIAL_EXPIRATION\n')
-      writeSync(3, `export AWS_PROFILE=${args.profile}\n`)
+      writeSync(3, `export AWS_PROFILE=${profile}\n`)
     } else if (args.export === 'creds') {
       writeSync(3, 'unset AWS_PROFILE\n')
       const { stdout } = await exec(
-        `aws configure export-credentials --profile ${args.profile} --format env`
+        `aws configure export-credentials --profile ${profile} --format env`
       )
       writeSync(3, stdout)
     } else {
